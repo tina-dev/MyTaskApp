ca849ff808a5a5dfe3a4391db7e26d28f8d14669
Tina <tina.temishova@gmail.com>
2018-05-20 11:09:26 +0600

    Project

---
+++ src/main/java/net/tina/MyTaskApp/controller/LoginController.java
@@ -0,0 +1,15 @@
+package net.tina.MyTaskApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController
+{
+	@RequestMapping(value = {"", "/", "/login"}, method = RequestMethod.GET)
+	public String getLoginPage()
+	{
+		return "login";
+	}
+}
---
+++ src/test/java/net/tina/MyTaskApp/MyTaskAppApplicationTests.java
@@ -0,0 +1,16 @@
+package net.tina.MyTaskApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTaskAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
