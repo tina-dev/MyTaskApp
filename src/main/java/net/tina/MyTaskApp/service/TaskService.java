77f28c2574b91c3fc4bf7157a176b1a7e7120e92
Tina <tina.temishova@gmail.com>
2018-05-21 19:22:06 +0600

    Created controllers, service and repos for tasks.

--- src/main/java/net/tina/MyTaskApp/service/TaskService.java
+++ src/main/java/net/tina/MyTaskApp/service/TaskService.java
@@ -1,14 +1,36 @@
 package net.tina.MyTaskApp.service;
+
 import java.util.List;
+
+import net.tina.MyTaskApp.entity.Milestone;
 import net.tina.MyTaskApp.entity.Task;
+import net.tina.MyTaskApp.entity.TaskUser;
 
 public interface TaskService
 {
 	public List<Task> getAllTasks();
+	
+	public List<Task> getTasksByUserId(int id);
 	
 	public Task createTask(Task task);
 	
 	public int updateTask(Task task);
 	
 	public void deleteTask(int id);
+	
+	public List<Milestone> getAllMilestones();
+	
+	public Milestone createMilestone(Milestone milestone);
+	
+	public int updateMilestone(Milestone milestone);
+	
+	public void deleteMilestone(int id);
+	
+	public List<TaskUser> getAllTaskUsers();
+	
+	public TaskUser createTaskUser(TaskUser taskUser);
+	
+	public int updateTaskUser(TaskUser taskUser);
+	
+	public void deleteTaskUser(int id);
 }
